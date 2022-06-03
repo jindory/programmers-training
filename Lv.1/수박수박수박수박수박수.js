8655f6dd7056fdd76de120a13fe6bdefa5c87204
jindory <laonjay@gmail.com>
2022-06-03 23:58:04 +0900

    문제풀이

--- Lv.1/수박수박수박수박수박수.js
+++ Lv.1/수박수박수박수박수박수.js
@@ -1,1 +1,9 @@
-으윽..일단 잔디심어
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, 
+// solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+// 2022-06-03 메모:초심자의 코드... 다음에 더 멋진 코드로 다시 푼다..🥺
+function solution(n) {
+    var answer = '수박';
+    let newAnswer= answer.repeat(n);
+    return newAnswer.slice(0, n);;
+}
